dff2cc34ebc6b46bbd0a0fc8f47983538756a9c5
LucasCastro100 <lucascastro121295@gmail.com>
2025-05-03 21:22:20 -0300

    alterações

--- MarlosRamos/resources/views/layouts/navigation.blade.php
+++ MarlosRamos/resources/views/layouts/navigation.blade.php
@@ -1,4 +1,5 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 {{ auth()->check() && auth()->user()->role == 1 ? 'md:ml-64' : 'w-full' }}">
+{{-- <nav x-data="{ open: false }" class="bg-white border-b border-gray-100 {{ auth()->check() && auth()->user()->role == 1 ? 'md:ml-64' : 'w-full' }}"> --}}
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
