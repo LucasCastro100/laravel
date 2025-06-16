4812db9f02df5be748499d091863785c7685a8ec
LucasCastro100 <lucascastro121295@gmail.com>
2025-06-16 15:46:17 -0300

    subindo crons

--- MarlosRamos/laravel/routes/console.php
+++ MarlosRamos/laravel/routes/console.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('app:check-cron')->cron('*/1 * * * *')->timezone('America/Sao_Paulo');
